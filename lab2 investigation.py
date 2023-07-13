699a20e616ec1d6388f4ee2065c8483c43f0d9a9
soardev <soardev>
2023-07-13 19:37:34 +0000

    logic 1

--- lab2 investigation.py
+++ lab2 investigation.py
@@ -49,7 +49,7 @@ def locate_source(action=None, success=None, container=None, results=None, handl
     ## Custom Code End
     ################################################################################
 
-    phantom.act("geolocate ip", parameters=parameters, name="locate_source", assets=["maxmind"], callback=join_debug_4)
+    phantom.act("geolocate ip", parameters=parameters, name="locate_source", assets=["maxmind"], callback=join_check_positives)
 
     return
 
@@ -122,7 +122,7 @@ def source_reputation(action=None, success=None, container=None, results=None, h
     ## Custom Code End
     ################################################################################
 
-    phantom.act("domain reputation", parameters=parameters, name="source_reputation", assets=["virustotal"], callback=join_debug_4)
+    phantom.act("domain reputation", parameters=parameters, name="source_reputation", assets=["virustotal"], callback=join_check_positives)
 
     return
 
@@ -155,18 +155,7 @@ def virus_search(action=None, success=None, container=None, results=None, handle
     ## Custom Code End
     ################################################################################
 
-    phantom.act("file reputation", parameters=parameters, name="virus_search", assets=["virustotal"], callback=join_debug_4)
-
-    return
-
-
-@phantom.playbook_block()
-def join_debug_4(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
-    phantom.debug("join_debug_4() called")
-
-    if phantom.completed(action_names=["locate_source", "source_reputation", "virus_search"]):
-        # call connected block "debug_4"
-        debug_4(container=container, handle=handle)
+    phantom.act("file reputation", parameters=parameters, name="virus_search", assets=["virustotal"], callback=join_check_positives)
 
     return
 
@@ -207,6 +196,71 @@ def debug_4(action=None, success=None, container=None, results=None, handle=None
     ################################################################################
 
     phantom.custom_function(custom_function="community/debug", parameters=parameters, name="debug_4")
+
+    return
+
+
+@phantom.playbook_block()
+def join_check_positives(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
+    phantom.debug("join_check_positives() called")
+
+    if phantom.completed(action_names=["locate_source", "source_reputation", "virus_search"]):
+        # call connected block "check_positives"
+        check_positives(container=container, handle=handle)
+
+    return
+
+
+@phantom.playbook_block()
+def check_positives(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
+    phantom.debug("check_positives() called")
+
+    # check for 'if' condition 1
+    found_match_1 = phantom.decision(
+        container=container,
+        conditions=[
+            ["virus_search:action_research.summary.positives", ">", 10]
+        ],
+        delimiter=None)
+
+    # call connected blocks if condition 1 matched
+    if found_match_1:
+        notify_soc_management(action=action, success=success, container=container, results=results, handle=handle)
+        return
+
+    return
+
+
+@phantom.playbook_block()
+def notify_soc_management(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
+    phantom.debug("notify_soc_management() called")
+
+    # set user and message variables for phantom.prompt call
+
+    user = container.get('owner_name', None)
+    role = None
+    message = """A potentially malicious file download has been detected on a local server with Ip address {0}."""
+
+    # parameter list for template variable replacement
+    parameters = [
+        "artifact:*.cef.destinationAddress"
+    ]
+
+    # responses
+    response_types = [
+        {
+            "prompt": "Notify SOC management?",
+            "options": {
+                "type": "list",
+                "choices": [
+                    "Yes",
+                    "No"
+                ],
+            },
+        }
+    ]
+
+    phantom.prompt2(container=container, user=user, role=role, message=message, respond_in_mins=30, name="notify_soc_management", parameters=parameters, response_types=response_types)
 
     return
 
